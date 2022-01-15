fb0dbfa61ea0b90e008d314d4a8db53db7643ddb
Ivy Do <ivy.do2011@gmail.com>
2022-01-15 14:01:25 -0500

    moved server.js in root

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,21 @@
 INSERT INTO department (name)
-VALUES ("Cast");
-INSERT INTO department (name)
-VALUES ("Production");
-INSERT INTO department (name)
-VALUES ("Designer");
-
+VALUES 
+    ("Cast"),
+    ("Production"),
+    ("Designer");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Actor", 100000, 1);
-VALUES ("Director", 150000, 2);
-VALUES ("Makeup Artist", 60000, 3);
-
+VALUES 
+    ("Actor", 100000, 1),
+    ("Director", 150000, 2),
+    ("Makeup Artist", 60000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Lili", "Reinhart", 1, 2);
-VALUES ("Cole", "Sprouse", 1, 2);
-VALUES ("Camila", "Mendes", 1, 2);
-VALUES ("Charles", "Melton", 1, 2);
-VALUES ("Madelaine", "Petsch", 1, 2);
-VALUES ("Steven ", "Adelson", 2, null);
-VALUES ("Erin", "Mackenzie", 3, 2);
-
-
+VALUES 
+    ("Lili", "Reinhart", 1, 2),
+    ("Cole", "Sprouse", 1, 2),
+    ("Camila", "Mendes", 1, 2),
+    ("Charles", "Melton", 1, 2),
+    ("Madelaine", "Petsch", 1, 2),
+    ("Steven ", "Adelson", 2, null),
+    ("Erin", "Mackenzie", 3, 2);
